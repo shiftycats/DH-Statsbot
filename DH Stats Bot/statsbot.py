fdc0cd278623b690d4a838149cadf71c071f7981
Johannes Wule <jwule2262@gmail.com>
2020-03-16 17:06:37 -0400

    Added a "favorite weapon" stat
    Added a "favorite weapon" stat that will display alongside the normal stats. This stat gets the name of the weapon a player has the most kills with and displays it to them along with the total number of kills they have with the weapon. Eg. "Favorite weapon: MP40 at 1350 kills." Most guns in the game are included along with grenades and the most-common high-kill tank/AT guns. More damage type entries will be added as they are needed but 99% of players' top weapons should already be included.
    
    Also removed the friendly fire stats to help fit the bot message on 1 line at the request of dirtybirdy. A friendly fire stat displaying the ratio between normal kills and friendly fire kills will be added soon.

--- DH Stats Bot/statsbot.py
+++ DH Stats Bot/statsbot.py
@@ -64,25 +64,111 @@ async def on_message(message):
             user_roid = user_dict[user_id]
 
             stats_url = "http://api.darklightgames.com/players/" \
-                         + user_roid + "/?format=json"
-
-            user_data = urllib.request.urlopen(stats_url)
-            stats_contents = user_data.read()
+                        + user_roid + "/?format=json"
+            
+            stats_data = urllib.request.urlopen(stats_url)
+            stats_contents = stats_data.read()
             load_stats = json.loads(stats_contents)
+            
+            weapon_url = "http://46.101.44.19/players/damage_type_kills/" \
+                         "?format=json&killer_id=" + user_roid + \
+                         "&limit=10&offset=0"
 
-            kills = "**Kills:** " + str(load_stats["kills"]) + " "
+            weapon_data = urllib.request.urlopen(weapon_url)
+            weapon_contents = weapon_data.read()
+            weapon_list = str(weapon_contents).split("{")
+
+            top_weapon = weapon_list[2]
+            split_list = top_weapon.split(",")
+            trimmed_list = [element.strip("}") for element in split_list]
+            del trimmed_list[-1]
+
+            stat_dict = {}
+
+            for element in trimmed_list:
+                (key, val) = element.split(":")
+                stat_dict[key] = val
+
+            dh_weapon_dict1 = {
+                '"DH_MG34DamType"'                : "MG34",
+                '"DH_MP40DamType"'                : "MP40",
+                '"DH_PPSH41DamType"'              : "PPSH-41 Drum",
+                '"DH_DP28DamType"'                : "DP28",
+                '"DH_30calDamType"'               : "30 Cal. MG",
+                '"DH_BARDamType"'                 : "BAR",
+                '"DH_ThompsonDamType"'            : "Thompson",
+                '"DH_MG42DamType"'                : "MG42",
+                '"DH_Kar98DamType"'               : "Kar98",
+                '"DH_M1GarandDamType"'            : "M1 Garand",
+                '"DH_StenMkIIDamType"'            : "Sten Mk. II",
+                '"DH_STG44DamType"'               : "STG 44",
+                '"DH_G43DamType"'                 : "Gewehr 43",
+                '"DH_M1CarbineDamType"'           : "M1 Carbine",
+                '"DH_GreaseGunDamType"'           : "M3 Grease Gun",
+                '"DH_SVT40DamType"'               : "SVT-40",
+                '"DH_EnfieldNo4DamType"'          : "Enfield No.4",
+                '"DH_PPD40DamType"'               : "PPD-40",
+                '"DH_M38DamType"'                 : "M38",
+                '"DH_BesaDamType"'                : "Besa",
+                '"DH_TT33DamType"'                : "TT-33",
+                '"DH_MN9130DamType"'              : "MN-9130",
+                '"DH_BrenDamType"'                : "Bren",
+                '"DH_Winchester1897DamType"'      : "Winchester 1897",
+                '"DH_PTRDDamType"'                : "PTRD-41",
+                '"DH_SpringfieldScopedDamType"'   : "Springfield",
+                '"DH_PPS43DamType"'               : "PPS-43",
+                '"DH_FG42DamType"'                : "FG42",
+                '"DH_Kar98ScopedDamType"'         : "Scoped Kar98",
+                '"DH_50CalDamType"'               : "50 Cal. MG",
+                '"DH_P38DamType"'                 : "Walther P38",
+                '"DH_G41DamType"'                 : "Gewehr 41",
+                '"DH_G43ScopedDamType"'           : "Scoped Gewehr 43",
+                '"DH_MP41DamType"'                : "MP41",
+                '"DH_ColtM1911DamType"'           : "Colt M1911",
+                '"DH_C96DamType"'                 : "Mauser C96",
+                '"DH_PPSH41_stickDamType"'        : "PPSH-41 35rnd",
+                '"DH_EnfieldNo2DamType"'          : "Enfield No.2",
+                '"DH_AVT40DamType"'               : "AVT-40",
+                '"DH_Nagant1895DamType"'          : "Nagant 1895",
+                '"DH_M44DamType"'                 : "MN-M44 Carbine",
+                '"DH_MN9130ScopedDamType"'        : "Scoped MN-9130",
+                '"DH_BazookaImpactDamType"'       : "Bazooka",
+                '"DH_PanzerschreckImpactDamType"' : "Panzerschreck",
+                '"DH_PanzerFaustImpactDamType"'   : "Panzerfaust"
+            }
+
+            dh_weapon_dict2 = {
+                '"DH_StielGranateDamType"'    : "Stielhandgranate",
+                '"DH_F1GrenadeDamType"'       : "F1 Grenade",
+                '"DH_M1GrenadeDamType"'       : "M1 Grenade",
+                '"DHShellHE75mmDamageType"'   : "75mm HE",
+                '"DHShellHE75mmATDamageType"' : "75mm HEAT",
+                '"DHArtillery105DamageType"'  : "Artillery 105mm",
+                '"DHShellHE105mmDamageType"'  : "105mm HE",
+                '"DHShellHE37mmDamageType"'   : "37mm HE",
+                '"DHShellHE50mmATDamageType"' : "50mm HEAT"
+            }
+
+            dh_weapon_dict1.update(dh_weapon_dict2)
+            
+            dam_type = stat_dict['"damage_type_id"']
+            ref_weapon = dh_weapon_dict1[dam_type]
+
+            fav_weapon = "**Favorite Weapon:** " + ref_weapon \
+                         + " at " + stat_dict['"kills"'] + " kills"
+
+            s_kills = "**Kills:** " + str(load_stats["kills"]) + " "
             deaths = "| **Deaths:** " + str(load_stats["deaths"]) + " "
             raw_kdr = str(load_stats["kills"] / load_stats["deaths"])
             kdr = "| **Kill-Death Ratio:** " + str(raw_kdr[0:4]) + " "
-            ff_kills = "| **FF Kills:** " + str(load_stats["ff_kills"]) + " "
-            ff_deaths = "| **FF Deaths:** " + str(load_stats["ff_deaths"]) + " "
-            stats = kills + deaths + kdr + ff_kills + ff_deaths
+            stats = " " + s_kills + deaths + kdr + "| " + fav_weapon
 
-            await message.channel.send(stats)
+            await message.channel.send(message.author.mention + stats)
 
         elif user_id not in user_dict:
             await message.channel.send("I couldn't find you in my storage!\n" \
                                         "Please make sure you added yourself first!")
+                                        
             await message.channel.send("Use `!commands`" \
                                         " to see a list of commands.")
 
